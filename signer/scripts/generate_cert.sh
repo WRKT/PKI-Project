8baab9d1298b81c03fbaa1d4c098f39fe9f2a223
Winness Rakotozafy <winness.rakotozafy@agoracalyce.com>
2024-08-30 16:07:48 +0200

    feat: add cert converter format for docker TLS

--- signer/scripts/generate_cert.sh
+++ signer/scripts/generate_cert.sh
@@ -17,5 +17,8 @@ openssl req -new -config /etc/ssl/openssl.cnf -keyout /etc/ssl/signer/private/${
 # Signing the CSR with CA certificate
 openssl x509 -req -in /etc/ssl/signer/${FQDN}.csr -CA /etc/ssl/ca/certs/ca.crt -CAkey /etc/ssl/ca/private/ca.key -CAcreateserial -out /etc/ssl/signer/certs/${FQDN}.crt -days 3650
 
+# Convert cert format (e.g: .cert for Docker)
+openssl x509 -inform PEM -in /etc/ssl/signer/certs/${FQDN}.crt -out /etc/ssl/signer/certs/${FQDN}.cert
+
 # Verify the certificate to ensure it's not self-signed
 openssl verify -CAfile /etc/ssl/ca/certs/ca.crt /etc/ssl/signer/certs/${FQDN}.crt
