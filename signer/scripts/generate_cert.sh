f35e2ffef3a18ec8792248848ad7bb06720f4894
Winness R. <winness.rakotozafy@outlook.fr>
2024-08-30 15:43:11 +0200

    feat: templatize openssl.cnf for signer container

--- signer/scripts/generate_cert.sh
+++ signer/scripts/generate_cert.sh
@@ -2,8 +2,17 @@
 
 set -e 
 
+# Check if FQDN is set
+if [ -z "$FQDN" ]; then
+  echo "Error: FQDN environment variable is not set."
+  exit 1
+fi
+
+# Replace the placeholder with the actual FQDN in the template
+sed "s/__FQDN__/${FQDN}/g" /etc/ssl/openssl.cnf.template > /etc/ssl/openssl.cnf
+
 mkdir -p /etc/ssl/signer/private /etc/ssl/signer/certs
-openssl req -new -config /etc/ssl/signer/openssl.cnf -keyout /etc/ssl/signer/private/${FQDN}.key -out /etc/ssl/signer/${FQDN}.csr -nodes
+openssl req -new -config /etc/ssl/openssl.cnf -keyout /etc/ssl/signer/private/${FQDN}.key -out /etc/ssl/signer/${FQDN}.csr -nodes
 
 # Signing the CSR with CA certificate
 openssl x509 -req -in /etc/ssl/signer/${FQDN}.csr -CA /etc/ssl/ca/certs/ca.crt -CAkey /etc/ssl/ca/private/ca.key -CAcreateserial -out /etc/ssl/signer/certs/${FQDN}.crt -days 3650
